1872a56905e6f9cc28161f28ee78b401daaed430
Harry Hutapea <harrybhutapea@gmail.com>
2025-09-07 18:23:10 +0700

    first try to fix font and color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-        "./index.html",
-        "./src/**/*.{js,ts,jsx,tsx}",
-    ],
-    theme: {
-        extend: {
-            colors: {
-                'primary-red': '#E4262C',
-                'secondary-red': '#B60811', // Example of a custom purple
-            },
-            fontFamily: {
-                'nabla': ['Nabla', 'sans-serif'],
-                'roboto': ['Roboto', 'sans-serif'],
-            },
-        },
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  safelist: ["font-nabla", "font-roboto", "text-primary-red", "text-secondary-red"],
+  theme: {
+    extend: {
+      colors: {
+        "primary-red": "#E4262C",
+        "secondary-red": "#B60811",
+      },
+      fontFamily: {
+        nabla: ["Nabla", "sans-serif"],
+        roboto: ["Roboto", "sans-serif"],
+      },
     },
-    plugins: [],
+  },
+  plugins: [],
 }
