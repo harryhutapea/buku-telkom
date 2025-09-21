1960196f83d3ea509129c228a8ce0a516c9decb8
Harry Hutapea <harrybhutapea@gmail.com>
2025-09-21 18:49:52 +0700

    final fix

---
+++ public/js/main.js
@@ -0,0 +1,6 @@
+function openOverlay() {
+    document.getElementById("overlay").classList.remove("hidden");
+}
+function closeOverlay() {
+    document.getElementById("overlay").classList.add("hidden");
+}
